c0c45673c9176992e287ca109ccf5c12c381d64a
tonni <32508133@qq.com>
2013-05-03 08:22:31 +0800

    测试001
    Change-Id: I2c20c3b0bca862b680fdc7fee415424b728ab175
    Signed-off-by: tonni <32508133@qq.com>

--- src/test/Controller.java
+++ src/test/Controller.java
@@ -20,5 +20,6 @@ public class Controller {
 				robotstxtServer); /*                 * For each crawl, you need to add some seed urls. These are the first                 * URLs that are fetched and then the crawler starts following links                 * which are found in these pages                 */
 		controller.addSeed("http://site-digger.com/html/feepolicy/"); /*                 * Start the crawl. This is a blocking operation, meaning that your code                 * will reach the line after this only when crawling is finished.                 */
 		controller.start(MyCrawler.class, numberOfCrawlers);
+		
 	}
 }
